edfdf0ee8cb8bc2fbaaf3a9abeee2f0d8e830397
Ricky Sierra <86573167+RickySG1@users.noreply.github.com>
2022-02-27 23:05:19 -0500

    Add files via upload

---
+++ sshscript.py
@@ -0,0 +1,28 @@
+import os
+import netmiko
+from netmiko import ConnectHandler
+from getpass import getpass
+from datetime import date
+
+username = input("Enter your SSH Username: ")
+password = getpass("Enter your SSH Password: ")
+date = date.today().strftime("%Y %m %d")
+
+device = {
+    'ip' : '192.168.108.10',
+    'username' : username,
+    'password' : password,
+    'device_type' : 'cisco_ios'
+}
+
+c = ConnectHandler(**device)
+
+output = c.send_command("show run")
+
+f = open(f'back_configuration{date}','x')
+
+f.write(output)
+f.close
+
+
+
